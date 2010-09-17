fdd968e0b6432b74721935c4ab6b5c16ba36386f
jvancleef <jvancleef@earthlink.net>
2010-09-17 17:14:29 -0400

    switched to 5 point system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,36 +18,46 @@ class PagesController < ApplicationController
   def create
 
    @userweight = Userweight.new(params[:userweight])
-   if
-    (@userweight.winweight.to_f + @userweight.ypgweight.to_f +
-     @userweight.yppweight.to_f + @userweight.tdweight.to_f +
-     @userweight.dypgweight.to_f + @userweight.dyppweight.to_f +
-     @userweight.dtdweight.to_f) == 100
+
+
 
      session[:userweight] = @userweight
-     redirect_to :action => :output
-   else
-     session[:error] = "Inputs do not equal 100 Percent"
-    redirect_to root_path
 
-   end
+     debugger
+     redirect_to :action => :output
+#   else
+#     session[:error] = "Inputs do not equal 100 Percent"
+#    redirect_to root_path
+
+#   end
 
   end
 
   def output
 
-  @userweight = session[:userweight]
+   @userweight = session[:userweight]
+   @totalpoints = (@userweight.winweight.to_f + @userweight.ypgweight.to_f +
+     @userweight.yppweight.to_f + @userweight.tdweight.to_f +
+     @userweight.dypgweight.to_f + @userweight.dyppweight.to_f +
+     @userweight.dtdweight.to_f)
+
+  ["winweight", "ypgweight", "yppweight", "yppweight", "tdweight", "dypgweight", "dyppweight",
+    "dtdweight"].each do |reweight|
+      eval("@userweight.#{reweight} = @userweight.#{reweight}.to_f / @totalpoints")
+    end
+
+
   @teamtable = []
     Totaloffense.all.each do |stats|
       @teamd = Totaldefense.find_by_team(stats.team)
       @teamtable << {:team => stats.team,
-                      :score => (((@userweight.winweight.to_f / 100) * stats.winpct) +
-                        ((@userweight.ypgweight.to_f / 100) * stats.yardspergamerr) +
-                        ((@userweight.yppweight.to_f / 100) * stats.yardsperplayrr) +
-                        ((@userweight.tdweight.to_f / 100) * stats.touchdownspergamerr)+
-                        ((@userweight.dypgweight.to_f / 100) * @teamd.dyardspergamerr) +
-                        ((@userweight.dyppweight.to_f / 100) * @teamd.dyardsperplayrr) +
-                        ((@userweight.dtdweight.to_f / 100) * @teamd.dtouchdownspergamerr)
+                      :score => (((@userweight.winweight.to_f ) * stats.winpct) +
+                        ((@userweight.ypgweight.to_f ) * stats.yardspergamerr) +
+                        ((@userweight.yppweight.to_f ) * stats.yardsperplayrr) +
+                        ((@userweight.tdweight.to_f ) * stats.touchdownspergamerr)+
+                        ((@userweight.dypgweight.to_f ) * @teamd.dyardspergamerr) +
+                        ((@userweight.dyppweight.to_f ) * @teamd.dyardsperplayrr) +
+                        ((@userweight.dtdweight.to_f ) * @teamd.dtouchdownspergamerr)
                         #add a + and put new weight lines here
                         ) }
     end
--- app/models/userweight.rb
+++ app/models/userweight.rb
@@ -20,12 +20,7 @@ class Userweight
   end
 
 
-  def scorecalc(weights, stats)
-    weights.userscore = ((weights.winweight / 100) * stats.winpct) +
-                        ((weights.ypgweight / 100) * stats.yardspergamerr) +
-                        ((weights.yppweight / 100) * stats.yardsperplayrr) +
-                        ((weights.tdweight / 100) * stats.touchdownspergamerr)
-  end
+
 
 end
 
